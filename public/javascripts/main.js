015d6c39e0dedf1a18c11b40c31f10a8de450be4
Jean-Baptiste Lemée <jblemee@yahoo.fr>
2011-05-14 16:16:45 +0200

    Route can't be on JS files

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,5 @@
-require(["jquery","widgets/likedlist", "widgets/likedaddbox", "widgets/login"], function($, LikedList, LikedAddBox, Login) {
-   var connected = null;
+var connected = null;
+require(["jquery","widgets/likedlist", "widgets/likedaddbox", "widgets/login",  "jquery.history", "openid-jquery", "functional"], function($, LikedList, LikedAddBox, Login) {
 
    var setContentPage = function(html) {
       $('#page_content').html(html);
@@ -39,30 +39,5 @@ require(["jquery","widgets/likedlist", "widgets/likedaddbox", "widgets/login"], 
          $.history.load(url);
          return false;
       });
-
-      $('#search-button').click(function(e) {
-         e.preventDefault();
-         var searchText = $('#search-input').val();
-         LikedList.Instance('search-results', '@{Application.search()}', {limit:10,text: searchText}, '@{Reco.isLiked()}', '@{Reco.switchLike()}', '@{Reco.switchIgnore()}');
-      });
-      var recentsLiked = new LikedList.Instance('recents', '@{Application.lastAdded()}', {limit:10}, '@{Reco.isLiked()}', '@{Reco.switchLike()}', '@{Reco.switchIgnore()}');
-      var popularsLiked = new LikedList.Instance('populars', '@{Application.mostLiked()}', {limit:10}, '@{Reco.isLiked()}', '@{Reco.switchLike()}', '@{Reco.switchIgnore()}');
-      var relatedLiked = new LikedList.Instance('related', '@{Application.recommendFromLiked()}', {limit:10}, '@{Reco.isLiked()}', '@{Reco.switchLike()}', '@{Reco.switchIgnore()}');
-
-      var onConnectedUser = function(username) {
-         Login.withConnectedUserDefault(username);
-         LikedList.Instance('user-recommendations', '@{Reco.recommendUser()}', {limit:10}, '@{Reco.isLiked()}', '@{Reco.switchLike()}', '@{Reco.switchIgnore()}');
-         var likedAddBox = new LikedAddBox.Instance('add-liked', '@{Reco.addLiked()}');
-         likedAddBox.onLikedAdded.add(recentsLiked.refresh);
-         likedAddBox.onLikedAdded.add(popularsLiked.refresh);
-      }
-
-      recentsLiked.onClickLiked.add(relatedLiked.refresh);
-      popularsLiked.onClickLiked.add(relatedLiked.refresh);
-      relatedLiked.onClickLiked.add(relatedLiked.refresh);
-
-      Login.init('@{Security.userName()}', onConnectedUser);
-
-      FB.init({appId: '161437957254554', status: true, cookie: true, xfbml: true});
    });
 });
